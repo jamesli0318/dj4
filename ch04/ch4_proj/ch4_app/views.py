11cad0d75d8c8a3506fb918c5a941ca753bb7826
James Li <james_ch_li@wiwynn.com>
2024-09-05 22:37:00 +0800

    finish 4.4

--- ch04/ch4_proj/ch4_app/views.py
+++ ch04/ch4_proj/ch4_app/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, Http404
+from ch4_app.models import Product
+import random
 # Create your views here.
+def about (request):
+    quotes = ['To be or not to be is a question.',
+            'A dog jumps over a lazy fox.',
+            'Knowledge is power.']
+    quote = random.choice(quotes)
+
+    return render(request, 'about.html', locals())
+
+def listing(request):
+    products = Product.objects.all()
+    render(request, 'list.html', locals())
+    
+    return render(request, 'list.html', locals())
+
+def disp_detail(request, sku):
+    try:
+        p = Product.objects.get(sku=sku)
+    except Product.DoesNotExist:
+        raise Http404('Not found item.')
+    
+    return render(request, 'disp.html', locals())
